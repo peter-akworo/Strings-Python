7f3499e4ea0987697c93463b536e221bc7a11c10
peter-akworo <pittonyango@gmail.com>
2020-05-14 21:42:32 +0300

    Initial Commit

--- String.py
+++ String.py
@@ -1,3 +1,16 @@
+# Representing strings
+H1 = "Hello World!"  # print hello world
+print(H1)
 
+H2 = "Hello World!"  # print hello world
+print(H2)
 
-
+# Quotes usage
+print("It's the day")  # double quotes
+print('She said "Hello" to everyone')  # single quotes
+print(
+    """
+    Hello
+        World
+    """
+)  # triple quotes for multiline string support
