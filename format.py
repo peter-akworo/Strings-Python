b492f3443fa72f7ae2d9702dc37b64bc3f35e941
peter-akworo <pittonyango@gmail.com>
2020-05-14 21:22:59 +0300

    Final commit

--- format.py
+++ format.py
@@ -24,3 +24,36 @@ sentence5 = (
     f"{xylophone:.5}"  # number after . specifies precision length. Trancation length.
 )
 print(sentence5)
+
+# Truncating and padding
+Xylophone2 = "xylophone"
+sentence6 = f"{Xylophone2:10.5}"  # Trauncating at 5 and padded 10
+print(sentence6)
+
+# Simple number value conversion
+num1 = 42
+number1 = f"{num1:d}"  # integers
+print(number1)
+
+pi = 3.141592653589793
+number2 = f"{pi:f}"  # floats
+print(number2)
+
+# padding numbers
+num3 = 42
+number3 = f"{num3:4d}"
+print(number3)
+
+# padding and truncating numbers
+pi2 = 3.141592653589793
+number4 = f"{pi2:06.2f}"  # six characters with two decimal points
+print(number4)
+
+# signed numbers
+num4 = 42
+number5 = f"{num4:+d}"  # positive integer
+print(number5)
+
+num5 = -42
+number6 = f"{num5: d}"  # negative integer
+print(number6)
