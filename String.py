a1ff630912c5c7f2acbac9c9fb9dc600aac82dde
peter-akworo <pittonyango@gmail.com>
2020-05-16 19:08:28 +0300

    Representation, usage, type and concatenation

--- String.py
+++ String.py
@@ -14,3 +14,16 @@ print(
         World
     """
 )  # triple quotes for multiline string support
+
+# string type
+myvariable = "Bob"
+print(type(myvariable))  # print myvariable item type
+
+# string operations and functions
+# string concatenation
+string1 = "Good "
+string2 = "Day"
+string3 = string1 + string2
+print(string3)
+
+print("Hello " + "World!")
