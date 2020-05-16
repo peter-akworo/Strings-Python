96a33e38a8891c03cfdcd78fb6cedaf37a991b7d
peter-akworo <pittonyango@gmail.com>
2020-05-16 20:30:32 +0300

    length, access, subaccess, repeat, split, count strings

--- String.py
+++ String.py
@@ -27,3 +27,35 @@ string3 = string1 + string2
 print(string3)
 
 print("Hello " + "World!")
+
+# length of a string
+print(len(string3))  # finds the length(no. of characters contained in the string)
+
+# Accessing a character
+mystring = "Hello World"
+print(mystring[4])  # fourth index item value(o); zero indexed.
+print(mystring[8])  # eighth item value(r);zero indexed
+
+# Accessing subset of characters
+my_string = "Hello World"
+print(my_string[0:3])  # print zero to third index item values
+print(my_string[:5])  # print from zero to fifth index item value
+print(my_string[2:])  # print from second index item value to the nth index item value
+
+# Repeating strings
+print("*" * 10)  # multiplies asterisk times ten.
+print("Hi" * 10)  # multiplies Hi ten times
+
+# Splitting Strings- using the split() method
+title = "The Good, The bad, and the Ugly"
+print("Source string: ", title)
+print("Print using space")
+print(title.split(" "))  # splits the title using the spaces in the sentence
+print("Print using commas")
+print(title.split(","))  # splits the title using the commas in the sentence
+
+# Counting strings
+my_count = "counts the number of counts in this counts string that counts counts"
+print(
+    "My count is: ", my_count.count("count")
+)  # Count the sub-string 'counts' in the string 'my_count'
