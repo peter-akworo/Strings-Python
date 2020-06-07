e05d4a7aeabec58f8284b04d934112ef6bff1b3f
peter-akworo <pittonyango@gmail.com>
2020-06-07 17:18:06 +0300

    Integer conversion

--- NumbersBooleanNone.py
+++ NumbersBooleanNone.py
@@ -6,3 +6,11 @@ print(type(x))
 x2 = 1000000
 print(x2)
 print(type(x2))
+
+# Converting into integers
+total = int(
+    "100"
+)  # Convert a number or string x to an integer, or return 0 if no arguments are given.
+print(type(total))
+print(type(age))
+print(age)
